9b8baa069712ad568bd604402fad8a519bbe0d4f
Duke0404 <devansh0404@gmail.com>
2020-09-25 12:02:55 +0530

    v.03 works

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-var totalTime = new Date();
-var currentHours = totalTime.getHours();
-var currentMinutes = totalTime.getMinutes();
-var currentSeconds = totalTime.getSeconds();
-var clock1 = currentHours + ":" + currentMinutes + ":" + currentSeconds;
-document.getElementById("clock1").innerHTML = clock1;
+function updateClock () {
+    //Take all time data
+    var totalTime = new Date();
+
+    //Extract required data
+    var currentHours = totalTime.getHours();
+    var currentMinutes = totalTime.getMinutes();
+    var currentSeconds = totalTime.getSeconds();
+
+    //Format data
+    var clock1 = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+
+    //Print data
+    document.getElementById("clock1").innerHTML = clock1;
+}
