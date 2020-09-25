2fbebaf0efd29de2d7fea1123b1594d9790e255a
Duke0404 <devansh0404@gmail.com>
2020-09-25 10:06:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-var totalTime = new Date()
-var clock1 = totalTime.getHours() + ":" + totalTime.getMinutes() + ":" + totalTime.getHours() + ":"
+var totalTime = new Date();
+var clock1 = totalTime.getHours() + ":" + totalTime.getMinutes() + ":" + totalTime.getHours() + ":";
+document.getElementById("clock1").innerHTML = clock1;
